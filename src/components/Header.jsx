9cc5e78feb97f4abac6e0b016eef299fbb0b61f9
alisajad001 <forwebonly0@gmail.com>
2024-01-21 14:19:05 +0430

    Style: Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 const Header = () => {
-  return <h1>Fast Pizza Co.</h1>;
+  return (
+    <h1 className="text-center py-16 bg-yellow-500 text-5xl text-yellow-50 font-salsa uppercase shadow-sm">
+      Fast Pizza Co.
+    </h1>
+  );
 };
 
 export default Header;
